6d1df4056b9217e08f3b01e30e6990ed66c1699e
programmerit <chingovan@gmail.com>
2015-05-13 01:59:33 +0700

    Edit CAN and CANNode

--- CAN/src/vn/vnu/hus/mim/can/CAN.java
+++ CAN/src/vn/vnu/hus/mim/can/CAN.java
@@ -8,18 +8,32 @@ import java.util.TreeMap;
 public class CAN {
 	private List<CANNode>				nodeList		= new ArrayList<CANNode>();
 	private SortedMap<CANKey, CANNode>	sortedNodeMap	= new TreeMap<CANKey, CANNode>();
+	Object[]							sortedKeyArray;
 
 	public void createNode(String indentify) throws CANException {
 		CANNode node = new CANNode(indentify);
 
 		nodeList.add(node);
+
 		if (sortedNodeMap.get(node.getNodeKey()) != null) {
+
 			throw new CANException("Duplicated key: " + node);
 		}
 		sortedNodeMap.put(node.getNodeKey(), node);
 	}
 
 	public CANNode getNode(int i) {
+
 		return (CANNode) nodeList.get(i);
+	}
+
+	public CANNode getSortedNode(int i) {
+
+		if (sortedKeyArray == null) {
+
+			sortedKeyArray = sortedNodeMap.keySet().toArray();
+		}
+
+		return (CANNode) sortedNodeMap.get(sortedKeyArray[i]);
 	}
 }
--- CAN/src/vn/vnu/hus/mim/can/CANNode.java
+++ CAN/src/vn/vnu/hus/mim/can/CANNode.java
@@ -1,22 +1,64 @@
 package vn.vnu.hus.mim.can;
 
-public class CANNode {
-	private String	nodeId;
-	private CANKey	canKey;
+import java.util.LinkedList;
+import java.util.List;
 
-	private Zone	zone;
+import vn.vnu.hus.mim.can.util.NodeUtil;
+import vn.vnu.hus.mim.can.util.ZoneFactoryUtil;
+
+public class CANNode {
+	private String			nodeId;
+	private CANKey			canKey;
+
+	private Zone			zone;
+	private List<CANNode>	neighbors;
 
 	public CANNode(String nodeId, Zone zone) {
 		super();
 		this.nodeId = nodeId;
 		this.canKey = new CANKey(nodeId);
 		this.zone = zone;
+		neighbors = new LinkedList<CANNode>();
 	}
 
 	public CANNode(String nodeId) {
 		super();
 		this.nodeId = nodeId;
 		this.canKey = new CANKey(nodeId);
+		this.zone = ZoneFactoryUtil.getDefaultZone();
+		neighbors = new LinkedList<CANNode>();
+	}
+
+	/**
+	 * Join a {@link CANNode} into the hyper-rectangle
+	 * 
+	 * @author chinv
+	 * @param node
+	 *            The node which are joining into hyper-rectangle
+	 * */
+	public void join(CANNode node) {
+
+		CANNode nearestNode = NodeUtil.findNearestNode(this, node.getNodeKey());
+		
+		if( nearestNode != null) {
+			
+			try {
+				Zone[] zones = ZoneFactoryUtil.split( nearestNode.getZone());
+				
+				if( nearestNode.getNodeKey().getIdentifier().compareTo( node.getNodeKey().getIdentifier()) < 0) {
+					
+					nearestNode.setZone( zones[0]);
+					node.setZone( zones[1]);
+				} else {
+					
+					nearestNode.setZone( zones[1]);
+					node.setZone( zones[0]);
+				}
+			} catch (CANException e) {
+
+				e.printStackTrace();
+			}
+		}
 	}
 
 	public Zone getZone() {
@@ -33,6 +75,10 @@ public class CANNode {
 
 	public CANKey getNodeKey() {
 		return this.canKey;
+	}
+
+	public List<CANNode> getNeighbors() {
+		return neighbors;
 	}
 
 	public CANNode findOwner(CANKey key) {
